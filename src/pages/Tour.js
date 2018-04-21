87c8c5f0116e6cd9a437a2a6e9ddf65dc254ad55
Alex Edward Ball <ballalexedward@gmail.com>
2018-04-20 22:28:45 -0700

    tour page beginning to be styled

--- src/pages/Tour.js
+++ src/pages/Tour.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, ScrollView} from 'react-native';
+import {Text, View, Image, StyleSheet, ScrollView} from 'react-native';
 
 export default class Tour extends Component {
     render() {
@@ -9,7 +9,20 @@ export default class Tour extends Component {
                 <View style={styles.headerFixed}>
                     Tour #1
                 </View>
-                <View style={styles.toursContainer}>
+                <View style={styles.tourContainer}>
+                    <ScrollView>
+                        <View style={styles.selectedTour}>
+                            <Image
+                                style={styles.selectedTourImg} />
+                            <View style={styles.selectedTourAudioInfoBox}>
+                            </View>
+                            <View style={styles.selectedTourText}>
+                                <Text>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas ullam quaerat eos cumque corporis omnis dolorum, voluptates voluptate fugit illum repellendus dolores assumenda repudiandae, at qui, nulla debitis quo voluptas quibusdam. Itaque, sint eos?
+                                </Text>
+                            </View>
+                        </View>
+                    </ScrollView>
                 </View>
             </View>
         );
@@ -26,8 +39,28 @@ const styles = StyleSheet.create({
         backgroundColor: 'violet'
 
     },
-    toursContainer: {
+    tourContainer: {
         flex: .9,
         backgroundColor: 'grey'
+    },
+    selectedTour: {
+        // flex: 1,
+        display: 'flex',
+        flexDirection: 'column',
+        backgroundColor: 'red',
+        margin: 10,
+        height: 900
+    },
+    selectedTourImg: {
+        flex: .4,
+        backgroundColor: 'lightblue'
+    },
+    selectedTourAudioInfoBox: {
+        flex: .1,
+        backgroundColor: 'lightgrey'
+    },
+    selectedTourText: {
+        flex: 1,
+        backgroundColor: 'white'
     }
 })
