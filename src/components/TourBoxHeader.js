63490bb5b1e304c6452e2967ff4661ae1812927d
Alex Edward Ball <ballalexedward@gmail.com>
2018-05-12 20:09:22 -0700

    on press getting correct info will be passed down by props

--- src/components/TourBoxHeader.js
+++ src/components/TourBoxHeader.js
@@ -14,7 +14,7 @@ import Button from '../components/Button';
 export default class TourBoxHeader extends Component {
 
     _information = () => {
-        console.log('information');
+        console.log(this.props.title);
     }
 
     render() {
