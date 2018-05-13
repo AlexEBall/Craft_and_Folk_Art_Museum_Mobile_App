e0968d6c676dd1e60246a44d9d0380ba6548af9c
Alex Edward Ball <ballalexedward@gmail.com>
2018-05-12 20:02:37 -0700

    able to add svgs

--- src/components/TourBoxHeader.js
+++ src/components/TourBoxHeader.js
@@ -5,8 +5,10 @@ import {
     Image,
     StyleSheet,
     ScrollView,
+    TouchableOpacity,
     ImageBackground
 } from 'react-native';
+import Svg, { Path } from 'react-native-svg';
 import Button from '../components/Button';
 
 export default class TourBoxHeader extends Component {
@@ -19,9 +21,15 @@ export default class TourBoxHeader extends Component {
                     </Text>
                 </View>
                 <View style={styles.tourBoxButton}>
-                    <Button>
-                        More
-                    </Button>
+                    <TouchableOpacity style={styles.infoBtn}>
+                        <Svg height="32" width="32">
+                            <Path d="M16.5 29v0c-6.904 0-12.5-5.596-12.5-12.5s5.596-12.5 12.5-12.5c6.904 0 12.5 5.596 12.5 12.5s-5.596 12.5-12.5 12.5zM17 22v-7h-3v1h2v6h-2v1h5v-1h-2zM16.5 13c0.828 0 1.5-0.672 1.5-1.5s-0.672-1.5-1.5-1.5c-0.828 0-1.5 0.672-1.5 1.5s0.672 1.5 1.5 1.5v0z"
+                                fill='#ffffff'
+                                stroke='#009FB7'
+                                stokeWidth={5}>
+                            </Path>
+                        </Svg>
+                    </TouchableOpacity>
                 </View>
             </View>
         );
@@ -31,23 +39,23 @@ export default class TourBoxHeader extends Component {
 const styles = StyleSheet.create({
     tourBoxHeader: {
         flex: .2,
-        backgroundColor: '#F9D24E',
-        borderTopColor: '#009FB7',
-        borderTopWidth: 2,
-        borderBottomColor: '#009FB7',
-        borderBottomWidth: 2,
-        shadowColor: '#000',
-		shadowOffset: { width: 1, heigh: 2 },
-		shadowOpacity: 0.3,
-		shadowRadius: 2,
+        // backgroundColor: '#F9D24E',
+        // borderTopColor: '#009FB7',
+        // borderTopWidth: 2,
+        // borderBottomColor: '#009FB7',
+        // borderBottomWidth: 2,
+        // shadowColor: '#000',
+		// shadowOffset: { width: 1, heigh: 2 },
+		// shadowOpacity: 0.3,
+		// shadowRadius: 2,
         display: 'flex',
         flexDirection: 'row',
-        justifyContent: 'space-between',
         borderRadius: 10
     },
     tourBoxHeaderTitle: {
-        flexBasis: '50%',
+        flex: .8,
         paddingLeft: 10,
+        display: 'flex',
         alignItems: 'flex-start',
         justifyContent: 'center'
     },
@@ -59,6 +67,20 @@ const styles = StyleSheet.create({
         textShadowRadius: 1
     },
     tourBoxButton: {
-        flexBasis: '30%'
+        flex: .2,
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginRight: 10
+    },
+    infoBtn: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 50,
+        borderWidth: 1,
+        borderColor: '#FE4A49',
+        backgroundColor: '#009FB7'
     }
 });
