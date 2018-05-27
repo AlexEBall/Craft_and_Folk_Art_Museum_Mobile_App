0bf124f1f64d9f5354d9600aaa661dc1bcced668
Alex Edward Ball <ballalexedward@gmail.com>
2018-05-26 22:13:40 -0700

    one hard coded card done, need others and scroll view

--- src/pages/Programs.js
+++ src/pages/Programs.js
@@ -5,7 +5,9 @@ import {
     Image,
     StyleSheet,
     TouchableOpacity,
-    Linking
+    ScrollView,
+    Linking,
+    ImageBackground
 } from 'react-native';
 
 export default class Programs extends Component {
@@ -22,17 +24,33 @@ export default class Programs extends Component {
     render() {
         return (
             <View style={styles.container} onLayout={(event) => { this._findDimensions(event.nativeEvent.layout) }}>
+
                 <View style={styles.card}>
                     <View style={styles.mainImg}>
-                        <Image 
+                        <ImageBackground 
                             style={styles.img}
                             source={require('../assets/img/barm.png')}
-                            resizeMode='cover' />
+                            resizeMode='cover' >
+                            <View style={styles.priceBox}>
+                                <Text style={styles.price}>
+                                    $90
+                                </Text>
+                            </View>
+                        </ImageBackground>
                     </View>
                     <View style={styles.infoBox}>
-                        <Text>Hell0</Text>
+                        <View style={styles.infoBoxTitle}>
+                            <Text style={styles.title}>An Old-Fashioned Craft-Cocktail Workshop with Bar Mattachine</Text>
+                        </View>
+                        <View style={styles.infoBoxTime}>
+                            <Text style={styles.time}>Sunday, June 17. 2:00pm–4:00pm</Text>
+                        </View>
+                        <View style={styles.infoBoxDescription}>
+                            <Text style={styles.description}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum id aliquam sapiente, deleniti suscipit quaerat? Explicabo blanditiis placeat sint asperiores, harum aliquam, omnis repudiandae, rerum qui aut nostrum. Ullam, ratione.</Text>
+                        </View>
                     </View>
                 </View>
+
             </View>
         );
     }
@@ -55,16 +73,67 @@ const styles = StyleSheet.create({
         // alignItems: 'center'
     }, 
     mainImg: {
-        flex: .5
-    },
-    infoBox: {
         flex: .5,
         display: 'flex',
-        justifyContent: 'center'
+    },
+
+    priceBox: {
+        flex: .15,
+        display: 'flex',
+        backgroundColor: 'white',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        marginLeft: 15,
+        marginTop: 40,
+        width: 80,
+        // height: 100
+    },
+    price: {
+        fontSize: 28,
+        // padding: 
+    },
+
+
+    infoBox: {
+        flex: .5,
+        display: 'flex'
     },
     img: {
         flex: 1,
         height: null,
         width: null
+    },
+    infoBoxTitle: {
+        flex: 1,
+        backgroundColor: '#fff',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        
+        // marginLeft: 10
+    },
+    infoBoxTime: {
+        flex: .2,
+        backgroundColor: '#fff'
+    },
+    infoBoxDescription: {
+        flex: 1,
+        backgroundColor: '#fff'
+    },
+    title: {
+        fontSize: 24,
+        letterSpacing: 1.3,
+        paddingLeft: 15
+
+    },
+    time: {
+        fontSize: 16,
+        paddingLeft: 15,
+        color: '#4D4D4D'
+    },
+    description: {
+        fontSize: 20,
+        padding: 15
     }
 });
