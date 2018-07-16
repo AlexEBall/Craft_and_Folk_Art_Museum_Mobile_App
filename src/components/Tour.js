68e3fa13da7b615df25aa6369deff4fac5265406
Alex Edward Ball <ballalexedward@gmail.com>
2018-07-15 20:48:22 -0700

    switching to vertical scroll per cafam request, will work on artist social page now

--- src/components/Tour.js
+++ src/components/Tour.js
@@ -30,25 +30,11 @@ export default class Tour extends Component {
                                 stokeWidth={5}>
                             </Path>
                         </Svg>
-                        <Svg height="32" width="32">
-                            <Path d="M0 16c0 8.836 7.164 16 16 16 8.837 0 16-7.164 16-16 0-8.837-7.163-16-16-16s-16 7.163-16 16zM24 16l-8 8v-6h-8v-4h8v-6l8 8z"
-                                fill='#009FB7'
-                                stroke='#009FB7'
-                                stokeWidth={5}>
-                            </Path>
-                        </Svg>
                     </TouchableOpacity>
         } else {
             return  <TouchableOpacity style={styles.startBtn} onPress={this.onTourPress.bind(this)}>
                         <Svg height="32" width="32">
                             <Path d="M30 18h-2v-4c0-6.625-5.375-12-12-12s-12 5.375-12 12v4h-2c-1.105 0-2 0.895-2 2v8c0 1.105 0.895 2 2 2h6v-16c0-4.414 3.586-8 8-8s8 3.586 8 8v16h6c1.105 0 2-0.895 2-2v-8c0-1.105-0.895-2-2-2z"
-                                fill='#009FB7'
-                                stroke='#009FB7'
-                                stokeWidth={5}>
-                            </Path>
-                        </Svg>
-                        <Svg height="32" width="32">
-                            <Path d="M0 16c0 8.836 7.164 16 16 16 8.837 0 16-7.164 16-16 0-8.837-7.163-16-16-16s-16 7.163-16 16zM24 16l-8 8v-6h-8v-4h8v-6l8 8z"
                                 fill='#009FB7'
                                 stroke='#009FB7'
                                 stokeWidth={5}>
@@ -83,15 +69,17 @@ export default class Tour extends Component {
 
 const styles = StyleSheet.create({
     tour: {
-        width: 250,
+        height: 350,
+        // width: 250,
         backgroundColor: '#F08433',
-        marginRight: 5,
-        marginLeft: 5,
-        marginTop: 10,
-        marginBottom: 10,
-        borderRadius: 10,
+        // marginRight: 5,
+        // marginLeft: 5,
+        // marginTop: 10,
+        // marginBottom: 10,
+        // borderRadius: 10,
         display: 'flex',
-        flexDirection: 'column'
+        flexDirection: 'column',
+        flex: 1
     },
     tourImgBox: {
         flex: 1,
@@ -101,34 +89,36 @@ const styles = StyleSheet.create({
         height: null,
         width: null,
         overflow: 'hidden',
-        borderRadius: 10,
+        // borderRadius: 10,
     },
     tourInfoBox: {
-        flex: .3,
+        flex: .2,
         display: 'flex',
         flexDirection: 'row',
+        justifyContent: 'flex-end',
         alignItems: 'center',
         backgroundColor: 'rgba(240, 132, 51, .7)'
     },
     push: {
-        flex: .7
+        flex: .8
     },
     startBtn: {
-        flex: 1,
+        // flex: 1,
         zIndex: 5,
-        flexDirection: 'row',
+        // width: 30,
+        // flexDirection: 'row',
         alignSelf: 'center',
-        justifyContent: 'space-around',
+        // justifyContent: 'space-around',
         opacity: 1,
         backgroundColor: '#fff',
-        borderRadius: 5,
+        borderRadius: 50,
         borderWidth: 1,
         borderColor: '#009FB7',
         margin: 15,
-        padding: 5
+        padding: 12
     },
     startBtnDisabled: {
-        flex: 1,
+        // flex: 1,
         zIndex: 5,
         opacity: .5,
         backgroundColor: '#fff',
@@ -139,12 +129,29 @@ const styles = StyleSheet.create({
         padding: 5
     },
     imgText: {
+        // display: flex
         margin: 10,
         color: 'white',
-        fontSize: 24,
+        fontSize: 28,
         letterSpacing: 2,
         textShadowOffset: { width: 2, height: 2 },
         textShadowColor: 'black',
         textShadowRadius: 1,
     }
-})
+});
+
+// <Svg height="32" width="32">
+//                             <Path d="M0 16c0 8.836 7.164 16 16 16 8.837 0 16-7.164 16-16 0-8.837-7.163-16-16-16s-16 7.163-16 16zM24 16l-8 8v-6h-8v-4h8v-6l8 8z"
+//                                 fill='#009FB7'
+//                                 stroke='#009FB7'
+//                                 stokeWidth={5}>
+//                             </Path>
+//                         </Svg>
+
+                        // <Svg height="32" width="32">
+                        //     <Path d="M0 16c0 8.836 7.164 16 16 16 8.837 0 16-7.164 16-16 0-8.837-7.163-16-16-16s-16 7.163-16 16zM24 16l-8 8v-6h-8v-4h8v-6l8 8z"
+                        //         fill='#009FB7'
+                        //         stroke='#009FB7'
+                        //         stokeWidth={5}>
+                        //     </Path>
+                        // </Svg>
--- src/components/TourBox.js
+++ src/components/TourBox.js
@@ -7,7 +7,7 @@ import {
     ScrollView,
     ImageBackground
 } from 'react-native';
-import TourBoxHeader from '../components/TourBoxHeader';
+// import TourBoxHeader from '../components/TourBoxHeader';
 import Tour from '../components/Tour';
 
 export default class TourBox extends Component {
@@ -21,10 +21,8 @@ export default class TourBox extends Component {
     render() {
         return (
             <View style={styles.tourBox}>
-                <TourBoxHeader
-                title={this.props.tour.tourAudience} />
                 <View style={styles.tourBoxTours}>
-                    <ScrollView horizontal={true}>
+                    <ScrollView>
                         {this.renderTour()}
                     </ScrollView>
                 </View>
@@ -38,16 +36,23 @@ const styles = StyleSheet.create({
         // flex: 1,
         display: 'flex',
         flexDirection: 'column',
-        marginTop: 10,
-        marginBottom: 10,
-        marginRight: 5,
-        marginLeft: 5,
-        height: 350
+        // marginTop: 10,
+        // marginBottom: 10,
+        // marginRight: 5,
+        // marginLeft: 5,
+        // height: 'auto',
+        // borderColor: 'green',
+        // borderWidth: 2
     },
     tourBoxTours: {
-        flex: .8,
+        flex: 1,
         backgroundColor: '#ffffff',
         display: 'flex',
-        flexDirection: 'row'
+        // flexDirection: 'row',
+        // borderColor: 'black',
+        // borderWidth: 2
     }
-})
+});
+
+// for tour header
+// <TourBoxHeader title={this.props.tour.tourAudience} /> 
