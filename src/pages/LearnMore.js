a117ff450e4baf1326c5df6d44d78bb5e812b785
Alex Edward Ball <ballalexedward@gmail.com>
2018-07-18 11:51:49 -0700

    work on artist connect title color, what works best?

--- src/pages/LearnMore.js
+++ src/pages/LearnMore.js
@@ -121,7 +121,7 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: '600',
         padding: 10,
-        color: '#fff',
+        color: '#f08433',
         textShadowOffset: { width: 2, height: 2 },
         textShadowColor: 'black',
         textShadowRadius: 1,
